dd4e6cd0dcbab9facd970c62a63bc75f76a27950
lgxxhi <luisgsilva95@gmail.com>
2023-08-20 23:47:44 -0400

    small change

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="text-center mt-3">
+      <Link to={"/invoices"}>
+        <button className="btn btn-outline-dark p-2">Invoices</button>
+      </Link>
+    </div>
+  );
 }
 
 export default Home;
